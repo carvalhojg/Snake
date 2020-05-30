1a7b34a68c8ab02cc43a578ac8099b9128222f5d
Joao Gabriel Carvalho <joaoocarvalhol.santos@gmail.com>
2020-05-30 11:13:13 -0700

    first commit

---
+++ snake.py
@@ -0,0 +1,14 @@
+# Simplistic Snake Game
+
+import turtle
+
+# Screen
+wn = turtle.Screen()
+wn.title("Sanke Game by @carvalhojg")
+wn.bgcolor("green")
+wn.setup(width=600, height=600)
+wn.tracer(0)
+
+
+
+wn.mainloop()
