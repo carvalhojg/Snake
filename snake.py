ab5930085c7f01538d8419107983703a6645e228
Joao Gabriel Carvalho <joaoocarvalhol.santos@gmail.com>
2020-06-03 12:37:59 -0700

    add snake head

--- snake.py
+++ snake.py
@@ -1,14 +1,33 @@
 # Simplistic Snake Game
 
 import turtle
+import time
+import random
+
+delay = 0.1
 
 # Screen
 wn = turtle.Screen()
-wn.title("Sanke Game by @carvalhojg")
+wn.title("Snake Game by @carvalhojg")
 wn.bgcolor("green")
 wn.setup(width=600, height=600)
 wn.tracer(0)
 
+# Snake
+## Head
+head = turtle.Turtle()
+head.speed(0)
+head.shape("square")
+head.color("black")
+head.penup()
+head.goto(0, 0)
+head.direction = "stop"
+
+# Main game loop
+while True:
+    wn.update()
+
+    time.sleep(delay)
 
 
 wn.mainloop()
